89c0bf9a0432622bcb0d72a9070c8c1e28fa8ba6
Pavan Kumar Akula <akulapavankumar@outlook.com>
2023-09-07 16:46:11 +0200

    Add DRY to Reddis

--- reddis.sh
+++ reddis.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-DATE=$(date +%F)
-LOGSDIR=/tmp
-# /home/centos/shellscript-logs/script-name-date.log
-SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
-Y="\e[33m"
+source common.sh
 
-if [ $USERID -ne 0 ];
-then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1
-fi
+CHECK_ROOT
 
-VALIDATE(){
-    if [ $1 -ne 0 ];
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
+CREATE_LOG
+
+COLORS
 
 
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOGFILE
